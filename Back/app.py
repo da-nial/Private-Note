8b5b2005a2dbe1e1888de103e20dbe9ae045823f
amirhossein-rajabpour <rajabpouramirhosein@gmail.com>
2022-06-28 12:07:17 +0430

    connection to MongoDB established

--- Back/app.py
+++ Back/app.py
@@ -1,19 +1,29 @@
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, jsonify
+from flask_mongoengine import MongoEngine
+from mongoengine import *
+import json
 from datetime import datetime
 import uuid
 
 app = Flask(__name__)
 # TODO: connect to MongoDB
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
-db = SQLAlchemy(app)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'my_notes',
+    'host': 'localhost',
+    'port': 27017
+}
+db = MongoEngine()
+db.init_app(app)
 
 
-class Note(db.Model):
-    unique_url = db.Column(db.String(300), primary_key=True, nullable=False)
-    content = db.Column(db.String(500), nullable=False)
+class Note(db.Document):
+    unique_url = StringField(primary_key=True, required=True)
+    # unique_url = db.Column(db.String(300), primary_key=True, nullable=False)
+    content = StringField(required=True)
+    # content = db.Column(db.String(500), nullable=False)
     # TODO: this field should be filled from config file
-    date_expiration = db.Column(db.DateTime)
+    date_expiration = DateTimeField()
+    # date_expiration = db.Column(db.DateTime)
 
     def __repr__(self) -> str:
         rep_str = "Note: " + self.unique_url + "--- Content: " + self.content
@@ -31,18 +41,6 @@ def index():
     return "Index page"
 
 
-@app.route('/delete/<string:u_url>')
-def delete(u_url):
-    note_to_delete = Note.query.get_or_404(u_url)
-
-    try:
-        db.session.delete(note_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return "There was an issue deleting your Note!"
-
-
 @app.route('/create-note', methods=['POST'])
 def create_note():
     if request.method == "POST":
@@ -52,8 +50,9 @@ def create_note():
         new_note = Note(content=note_content, unique_url=new_url)
 
         try:
-            db.session.add(new_note)
-            db.session.commit()
+            new_note.save()
+            # db.session.add(new_note)
+            # db.session.commit()
             # TODO: return created url to the client
             return new_url
         except:
@@ -69,8 +68,23 @@ def note_warn(u_url):
 @app.route('/note/<string:u_url>', methods=['GET'])
 def show_note(u_url):
     # TODO: show note and then delete it
-    note = Note.query.get_or_404(u_url)
+    note = Note.objects(unique_url=u_url)
+    # note = Note.query.get_or_404(u_url)
     return note.__repr__()
+
+
+@app.route('/delete/<string:u_url>')
+def delete(u_url):
+    note_to_delete = Note.objects(unique_url=u_url)
+    # note_to_delete = Note.query.get_or_404(u_url)
+
+    try:
+        note_to_delete.delete()
+        # db.session.delete(note_to_delete)
+        # db.session.commit()
+        return redirect('/')
+    except:
+        return "There was an issue deleting your Note!"
 
 
 if __name__ == "__main__":
