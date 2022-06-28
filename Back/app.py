a47b52eca7711e2ca3813e1e84b22729cd9a895e
amirhossein-rajabpour <rajabpouramirhosein@gmail.com>
2022-06-28 19:41:42 +0430

    db works fine

--- Back/app.py
+++ Back/app.py
@@ -1,16 +1,13 @@
-from flask import Flask, request, redirect, jsonify
+from flask import Flask, request, redirect
 from flask_mongoengine import MongoEngine
-# from flask_pymongo import PyMongo
 from mongoengine import *
+from datetime import datetime, timedelta
+import os
 import json
-from datetime import datetime
 import uuid
 
 app = Flask(__name__)
-# TODO: connect to MongoDB
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/my_notes"
-# mongo = PyMongo(app)
-
+# connect to MongoDB
 app.config['MONGODB_SETTINGS'] = {
     'db': 'my_notes',
     'host': 'localhost',
@@ -22,21 +19,24 @@ db.init_app(app)
 
 class Note(db.Document):
     unique_url = StringField(primary_key=True, required=True)
-    # unique_url = db.Column(db.String(300), primary_key=True, nullable=False)
     content = StringField(required=True)
-    # content = db.Column(db.String(500), nullable=False)
-    # TODO: this field should be filled from config file
-    date_expiration = DateTimeField()
-    # date_expiration = db.Column(db.DateTime)
+    # this field should be filled from config file
+    date_expiration = DateTimeField(default=30)
 
     def __repr__(self) -> str:
         rep_str = "Note: " + self.unique_url + "--- Content: " + self.content
         return rep_str
 
 
-# TODO: review this function
+# review this function
 def generate_unique_url():
-    return uuid.uuid4()
+    return str(uuid.uuid4())
+
+
+def get_expiration_date(day):
+    ini_time_for_now = datetime.now()
+    future_date_after_n_days = ini_time_for_now + timedelta(days=day)
+    return future_date_after_n_days
 
 
 @app.route('/', methods=['GET'])
@@ -48,22 +48,22 @@ def index():
 @app.route('/create-note', methods=['POST'])
 def create_note():
     if request.method == "POST":
-        note_content = request.form['content']
+        note_content = request.json["content"]
         new_url = generate_unique_url()
-        # expiration_date = TODO: read from config file
-        new_note = Note(content=note_content, unique_url=new_url)
+        days = os.environ.get("note-expiration", default=10)
+        expiration_date = get_expiration_date(days)
+        new_note = Note(content=note_content, unique_url=new_url, date_expiration=expiration_date)
 
         try:
             new_note.save()
-            # db.session.add(new_note)
-            # db.session.commit()
             # TODO: return created url to the client
-            return new_url
-        except:
+            return str(new_url)
+        except Exception as e:
+            print(e)
             return "There was an issue adding your Note!"
 
 
-@app.route('/note/warn/<string:u_url>', methods=['GET'])
+@app.route('/note-warn/<string:u_url>', methods=['GET'])
 def note_warn(u_url):
     # TODO: show the warning page
     return "warning page for: " + u_url
@@ -73,21 +73,18 @@ def note_warn(u_url):
 def show_note(u_url):
     # TODO: show note and then delete it
     note = Note.objects(unique_url=u_url)
-    # note = Note.query.get_or_404(u_url)
     return note.__repr__()
 
 
 @app.route('/delete/<string:u_url>')
 def delete(u_url):
     note_to_delete = Note.objects(unique_url=u_url)
-    # note_to_delete = Note.query.get_or_404(u_url)
 
     try:
         note_to_delete.delete()
-        # db.session.delete(note_to_delete)
-        # db.session.commit()
         return redirect('/')
-    except:
+    except Exception as e:
+        print(e)
         return "There was an issue deleting your Note!"
 
 
